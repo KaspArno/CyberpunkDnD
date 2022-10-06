2f0c7c056d38cbc3143d58d0dd8f5070c2b64ec8
KaspArno <kasper.skjeggestad@gmail.com>
2022-10-06 22:45:30 +0200

    added utility views

--- cyberpunk/utilities/views.py
+++ cyberpunk/utilities/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse
+from django.template import loader
 
-# Create your views here.
+from utilities.models import Utility
+
+
+def index(request):
+    utilities = Utility.objects.all()
+    context = {'utilities': utilities}
+    return render(request, 'utilities/index.html', context)
+
+
+def detail(request, utility_id):
+    utility = get_object_or_404(Utility, pk=utility_id)
+    return render(request, 'utilities/detail.html', {'utility': utility})
