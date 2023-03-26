4947c5b59098e346165569c53b5bd15bb4d2dcb0
Kasper Skjeggestad <kasper.skjeggestad@gmail.com>
2023-03-26 15:10:30 +0200

    test heroku, static path

--- cyberpunk/cyberpunk/settings.py
+++ cyberpunk/cyberpunk/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = '/home/django/www-data/example.com/static/'
+STATIC_ROOT = '/static_root/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
