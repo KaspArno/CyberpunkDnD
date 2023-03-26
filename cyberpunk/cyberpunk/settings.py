719f0cf4f5dbc6dc493f5952f6bf9d9fdfd5db75
Kasper Skjeggestad <kasper.skjeggestad@gmail.com>
2023-03-26 15:05:01 +0200

    test heroku, static path

--- cyberpunk/cyberpunk/settings.py
+++ cyberpunk/cyberpunk/settings.py
@@ -126,8 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = '/home/django/www-data/example.com/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
